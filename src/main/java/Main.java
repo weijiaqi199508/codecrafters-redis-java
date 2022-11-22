f255342471d14f2a1aea3a1371caa2929cee3108
魏嘉琪 <weijiaqi@ysstech.com>
2022-11-22 16:59:32 +0800

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,9 +16,12 @@ public class Main {
             InputStream in = clientSocket.getInputStream();
             byte[] bytes = new byte[1024];
             int read = in.read(bytes);
-            System.out.println("接收到的消息" + new String(bytes, 0, read));
-            OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write("+PONG\r\n".getBytes());
+            while (read != -1) {
+                System.out.println("接收到的消息" + new String(bytes, 0, read));
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("+PONG\r\n".getBytes());
+            }
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
